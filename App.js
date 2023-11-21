79b8596b536bae7e8f30e880dcb1df51cfd75227
TanjimAnim <tanjimanim007@gmail.com>
2023-11-21 19:09:11 +0600

    tailwind css added

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "2px",
+          color: "black",
+        }}
+      >
+        wow I am a cool app dev
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "powderblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
